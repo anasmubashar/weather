06166ba5afb3e09358219eeca35ecb7b8d9173b7
Anas Mubashar <i222581@nu.edu.pk>
2024-07-09 20:35:38 +0500

    last commit

---
+++ src/data.js
@@ -0,0 +1,4 @@
+const Unsplash_Api_Key = 'XiMno4l_C8G89rdysNjzAYvRqSQvenlwTtO78w9GViI';
+const OpenWeatherMap_Api_Key = '51122b41f541e37ccc93611c7a8d7bbf';
+
+export {Unsplash_Api_Key, OpenWeatherMap_Api_Key};
